23b29f45b91b9eede088a8ddc9c721b7679c0a5c
yboucher97 <yboucher@opti-plex.ca>
2026-09-16 16:06:54 -0400

    Expand rescue interface choices

---
+++ app/rescue_v2.py
@@ -0,0 +1,8 @@
+"""Expanded rescue-port UI supporting ether1-10 and sfp-sfpplus1-24."""
+
+import html
+
+from fastapi import Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from app import core_compat_placeholder
