9974996d395169a111b0a094141ebdbb4e3c9ed3
yboucher97 <yboucher@opti-plex.ca>
2026-09-19 07:51:02 -0400

    Finalize safe Montreal timestamp conversion patterns

--- app/ui_time.py
+++ app/ui_time.py
@@ -15,11 +15,11 @@ from app import settings
 LOCAL_TZ = ZoneInfo(settings.TIMEZONE)
 DISPLAY_ZONE_NAME = "Montréal"
 _ISO_RE = re.compile(
-    r"(?<![\"\'=\w])"
+    r"""(?<!["'=\w])"""
     r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2}))"
 )
 _LEGACY_UTC_RE = re.compile(
-    r"(?<![\\w])"
+    r"""(?<!["'=\w])"""
     r"(\d{4}-\d{2}-\d{2})[ T](\d{2}:\d{2}(?::\d{2})?)\s+UTC\b"
 )
 
